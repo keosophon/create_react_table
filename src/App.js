277a60a182419f76630316312fd6752339fcad23
keosophon <keosophon8888@gmail.com>
2024-05-24 05:52:49 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Data from "./data.json";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function AddMember() {
+  return (
+    <form>
+      <input type="text" name="username" placeholder="enter usernamer" />
+      <input
+        type="text"
+        name="email"
+        placeholder="enter email"
+        className="ms-2"
+      />
+      <input
+        type="text"
+        name="mobile"
+        placeholder="enter mobile number"
+        className="ms-2"
+      />
+      <button className="ms-2">Add</button>
+    </form>
+  );
+}
 
 function App() {
+  const [data, setData] = useState(Data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mt-3">
+        <AddMember />
+      </div>
+      <div className="mt-3">
+        <table className="table table-bordered table-striped">
+          <thead className="thead-dark">
+            <th>Username</th>
+            <th>Email</th>
+            <th>Mobile</th>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr>
+                <td>{item.username}</td>
+
+                <td>{item.email}</td>
+
+                <td>{item.mobile}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
